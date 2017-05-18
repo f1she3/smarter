bbc60df681dfbb3531e433e3f1ef7514fcfedfc6
f1she3 <f1sher@gmx.com>
2017-05-18 19:35:48 +0200

    Minor changes

--- www/functions/block.func.php
+++ www/functions/block.func.php
@@ -6,3 +6,30 @@
 		mysqli_stmt_bind_param($query, 'sss', $current_user, $username, $reason);
 		mysqli_stmt_execute($query);
 	}
+	function block($username, $contact, $reason){
+		$mysqli = get_link();
+		$query = mysqli_prepare($mysqli, 'INSERT INTO blocked (sender, contact, reason) VALUES (?, ?, ?)');
+		mysqli_stmt_bind_param($query, 'sss', $username, $contact, $reason);
+		mysqli_stmt_execute($query);
+	}
+	function unblock($username, $contact){
+		$mysqli = get_link();
+		$query = mysqli_prepare($mysqli, 'DELETE FROM blocked WHERE BINARY sender = ? and contact = ?');
+		mysqli_stmt_bind_param($query, 'ss', $username, $contact);
+		mysqli_stmt_execute($query);
+	}
+	function display_blocked_users($username){
+		$mysqli = get_link();
+		$query = mysqli_prepare($mysqli, 'SELECT contact FROM blocked WHERE BINARY sender = ?');
+		mysqli_stmt_bind_param($query, 's', $username);
+		mysqli_stmt_execute($query);
+		mysqli_stmt_bind_result($query, $contact);
+		$i = 0;
+		echo "<h2 class=\"text-center page-header\">Membres bloqués<h2>";
+		while($result = mysqli_stmt_fetch($query)){
+			echo "<a class=\"btn btn-warning\" href=\"".constant('BASE_URL')."block&user=".$contact."\">".$contact."</a>";
+		}
+		if($i === 0){
+			echo "<p class=\"text-center\">Vous n'avez bloqué aucun membre</p>";
+		}
+	}
--- www/pages/block.php
+++ www/pages/block.php
@@ -60,9 +60,94 @@
 			
 			}else if(isset($_POST['refuse'])){
 				redirect('private&user='.$user);
+			if(!is_blocked($_SESSION['name'], $user)){
+				echo "<h2 class=\"text-center\">Bloquer ".$user." ?</h2>
+					<p class=\"text-center\"><span class=\"glyphicon glyphicon-info-sign\"></span> ".$user." ne pourra plus vous  envoyer de demandes</p>
+					<h4 class=\"text-center page-header col-xs-12\">Motif</h4>
+					<form method=\"POST\" action=\"\" class=\"col-xs-8 col-xs-offset-2 col-sm-4 col-sm-offset-4\">
+						<div class=\"radio\">
+							<label>
+								<input type=\"radio\" name=\"optradio\" value=\"1\" checked=\"checked\">".$reasons[1]."
+							</label>
+						</div>
+						<div class=\"radio\">
+							<label>
+								<input type=\"radio\" name=\"optradio\" value=\"2\">".$reasons[2]."
+							</label>
+						</div>
+						<div class=\"radio\">
+							<label>
+								<input type=\"radio\" name=\"optradio\" value=\"3\">".$reasons[3]."
+							</label>
+						</div><br><br>
+						<div class=\"pull-left\">
+							<button name=\"block\" class=\"btn btn-success\">
+								Oui <span class=\"glyphicon glyphicon-ok\"></span>
+							</button>
+						</div>
+						<div class=\"pull-right\">
+							<button name=\"no_block\" class=\"btn btn-danger\">
+								Non <span class=\"glyphicon glyphicon-remove\"></span>
+							</button>
+						</div>
+					</form>";
+				if(isset($_POST['block'])){
+					if(isset($_POST['optradio']) && !empty($_POST['optradio'])){
+						$optradio = $_POST['optradio'] = secure($_POST['optradio']);
+						switch($optradio){
+							case(1):
+								$reason = $reasons[1];
+								break;
+							case(2):
+								$reason = $reasons[2];
+								break;
+							case(3):
+								$reason = $reasons[3];
+								break;
+							default:
+								$reason = $reasons[1];
+								break;
+						}
+					}
+					block($_SESSION['name'], $user, $reason);
+					set_flash('danger', "<span class=\"glyphicon glyphicon-user\"></span><span class=\"glyphicon glyphicon-volume-off\"></span>");
+					redirect(3);
+				
+				}else if(isset($_POST['no_block'])){
+					redirect('private');
+				}
+				
+			}else{
+				echo "<h2 class=\"text-center\">Vous avez bloqué ".$user."</h2>
+					<p class=\"text-center\"><span class=\"glyphicon glyphicon-info-sign\"></span> ".$user." pourra à nouveau vous envoyer de demandes et parler avec vous</p>
+					<h4 class=\"text-center page-header\">Débloquer ".$user." ?</h4>
+					<form method=\"POST\" action=\"\" class=\"col-xs-8 col-xs-offset-2 col-sm-4 col-sm-offset-4\">
+						<div class=\"pull-left\">
+							<button name=\"unblock\" class=\"btn btn-success\">
+								Oui <span class=\"glyphicon glyphicon-ok\"></span>
+							</button>
+						</div>
+						<div class=\"pull-right\">
+							<button name=\"no_unblock\" class=\"btn btn-danger\">
+								Non <span class=\"glyphicon glyphicon-remove\"></span>
+							</button>
+						</div>
+					</form>";
+				if(isset($_POST['unblock'])){
+					unblock($_SESSION['name'], $user);
+					set_flash('success', "<span class=\"glyphicon glyphicon-user\"></span><span class=\"glyphicon glyphicon-volume-up\"></span>");
+					redirect('private&user='.$user);
+
+
+				}else if(isset($_POST['no_unblock'])){
+					redirect(3);
+				}
 			}
 		
 		}else{
 			set_error('Erreur', 'zoom-out', 'Cet utilisateur n\'éxiste pas', 'contacts');
 		}
+	
+	}else{
+		display_blocked_users($_SESSION['name']);
 	}
