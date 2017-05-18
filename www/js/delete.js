311ef83fadcddee169e36217611a7f07ed14a999
f1she3 <f1sher@gmx.com>
2017-05-18 19:32:41 +0200

    Minor changes

--- www/js/delete.js
+++ www/js/delete.js
@@ -3,33 +3,33 @@ $(document).ready(function(){
 		if(e.keyCode != 13){
 			$(".errors").removeClass("alert alert-danger");
 			$(".errors").addClass("hidden");
-			$('.errors-block').html("<div class='alert alert-danger invisible'>a</div>");		
+			$(".errors-block").html("<div class=\"alert alert-danger invisible\">a</div>");		
 		}	
 	})
 	$("button[name=submit]").click(function(){
-		if($("input[type=password]").val() != ''){
+		if($("input[type=password]").val() != ""){
 			if($("input[type=password]").val().length >= 6){
 				return true;
 			
 			}else{
-				$("input[type=password]").val('');
+				$("input[type=password]").val("");
 				$("input[type=password]").focus();
 				$(".errors").removeClass("alert alert-danger");
 				$(".errors").addClass("hidden");
-				$(".errors-block").html("<div class='alert alert-danger text-center'>\
-											<span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> le mot de passe est incorrect\
+				$(".errors-block").html("<div class=\"alert alert-danger text-center\">\
+											<span class=\"glyphicon glyphicon-exclamation-sign\" aria-hidden=\"true\"></span> le mot de passe est trop court\
 										</div>");
 				
 				return false;
 			}
 		
 		}else{
-			$("input[type=password]").val('');
+			$("input[type=password]").val("");
 			$("input[type=password]").focus();
 			$(".errors").removeClass("alert alert-danger");
 			$(".errors").addClass("hidden");
-			$(".errors-block").html("<div class='alert alert-danger text-center'>\
-										<span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> veuillez saisir votre mot de passe\
+			$(".errors-block").html("<div class=\"alert alert-danger text-center\">\
+										<span class=\"glyphicon glyphicon-exclamation-sign\" aria-hidden=\"true\"></span> veuillez saisir votre mot de passe\
 									</div>");
 				
 			return false;
