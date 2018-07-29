f8e1b92c06fa76444471353b3770152af960c344
f1sher <f1sher@gmx.com>
2018-07-30 00:51:33 +0200

    Converting connexion system

--- www/js/login.js
+++ www/js/login.js
@@ -1,33 +1,50 @@
 $(document).ready(function(){
-	$("#name_log").keyup(function(e){
+	var socket = io.connect('http://127.0.0.1:8080');
+	$('form').submit(function(event){
+		event.preventDefault();
+		if($('#name_log').val() === ''){
+			$('#name_log').focus();
+			return false;
+		}
+		if($('#password_log').val() === ''){
+			$('#password_log').focus();
+			return false;
+		}
+		socket.emit('login', {
+			name	: $('#name_log').val(),
+			password : $('#password_log').val()
+		})
+	})
+/*
+	$('#name_log').keyup(function(e){
 		if(e.keyCode != 13){
 			// Removes the errors when the user types something
-			$(".errors").removeClass("alert alert-danger");
-			$(".errors").addClass("hidden");
-			$('.errors-block').html("<div class='alert alert-danger invisible'>a</div>");		
+			$('.errors').removeClass('alert alert-danger');
+			$('.errors').addClass('hidden');
+			$('.errors-block').html("<div class='alert alert-danger invisible'>a</div>");
 		}	
 	})
-	$("#password_log").keyup(function(e){
+	$('#password_log').keyup(function(e){
 		if(e.keyCode != 13){
-			$(".errors").removeClass("alert alert-danger center-block");
-			$(".errors").addClass("hidden");
+			$('.errors').removeClass('alert alert-danger center-block');
+			$('.errors').addClass('hidden');
 			$('.errors-block').html("<div class='alert alert-danger invisible'>a</div>");
 		}	
 	})
 	
-	$("#submit_log").click(function(){
-		if($("#name_log").val() != ''){
-			if($("#password_log").val() != ''){
-				if($("#password_log").val().length >= 6){
-					if($("#name_log").val().length >= 4){
+	$('#submit_log').click(function(){
+		if($('#name_log').val() != ''){
+			if($('#password_log').val() != ''){
+				if($('#password_log').val().length >= 6){
+					if($('#name_log').val().length >= 4){
 						return true;
 					
 					}else{
-						$("#name_log").val('');
-						$("#name_log").focus();
-						$(".errors").removeClass("alert alert-danger");
-						$(".errors").addClass("hidden");
-						$(".errors-block").html("<div class='alert alert-danger center-block text-center'>\
+						$('#name_log').val('');
+						$('#name_log').focus();
+						$('.errors').removeClass('alert alert-danger');
+						$('.errors').addClass('hidden');
+						$('.errors-block').html("<div class='alert alert-danger center-block text-center'>\n
 													<span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> le nom d'utilisateur est trop court\
 												</div>");
 				
@@ -35,11 +52,11 @@ $(document).ready(function(){
 					}
 				
 				}else{
-					$("#password_log").val('');
-					$("#password_log").focus();
-					$(".errors").removeClass("alert alert-danger");
-					$(".errors").addClass("hidden");
-					$(".errors-block").html("<div class='alert alert-danger center-block text-center'>\
+					$('#password_log').val('');
+					$('#password_log').focus();
+					$('.errors').removeClass('alert alert-danger');
+					$('.errors').addClass('hidden');
+					$('.errors-block').html("<div class='alert alert-danger center-block text-center'>\
 												<span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> le mot de passe est trop court\
 											</div>");
 				
@@ -47,10 +64,10 @@ $(document).ready(function(){
 				}
 			
 			}else{
-				$("#password_log").focus();
-				$(".errors").removeClass("alert alert-danger");
-				$(".errors").addClass("hidden");
-				$(".errors-block").html("<div class='alert alert-danger center-block text-center'>\
+				$('#password_log').focus();
+				$('.errors').removeClass('alert alert-danger');
+				$('.errors').addClass('hidden');
+				$('.errors-block').html("<div class='alert alert-danger center-block text-center'>\
 											<span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> veuillez saisir votre mot de passe\
 										</div>");
 				
@@ -58,14 +75,15 @@ $(document).ready(function(){
 			}
 			
 		}else{
-			$("#name_log").focus();
-			$(".errors").removeClass("alert alert-danger");
-			$(".errors").addClass("hidden");
-			$(".errors-block").html("<div class='alert alert-danger text-center'>\
+			$('#name_log').focus();
+			$('.errors').removeClass('alert alert-danger');
+			$('.errors').addClass('hidden');
+			$('.errors-block').html("<div class='alert alert-danger text-center'>\
 										<span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span> veuillez saisir votre pseudo\
 									</div>");
 		
 			return false;
 		}			
 	})	
+*/
 })
