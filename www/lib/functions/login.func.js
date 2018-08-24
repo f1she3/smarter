0e2f95b79507b606d0ddfd50bc48b5b3ea2e05d1
f1sher <f1sher@gmx.com>
2018-08-25 00:58:05 +0200

    Minor BUG fixing and refactoring

--- www/lib/functions/login.func.js
+++ www/lib/functions/login.func.js
@@ -1,39 +1,38 @@
 module.exports = authCheck = function(username, password, callback){
-	getCon((error, connection) =>{
+	return getCon((error, connection) =>{
 		if(error){
 			var errObj = new Error(error.code);
 			errObj.name = 'server';
 
 			return callback(errObj, false);
-		}else{
-			connection.query('SELECT password FROM users WHERE BINARY name = ?', [username], function(err, dbRes, fields){
-				if(err){
-					if(callback !== undefined){
-						var errObj = new Error(err.code);
-						errObj.name = 'server';
-
-						callback(errObj, false);
-					}else{
-						throw err;
-					}
-				// Valid username
-				}else if(dbRes.length > 0){
-					var bcrypt = require('bcrypt');
-					var passwordStr = JSON.stringify(dbRes[0]);
-					var passwordStrObj = JSON.parse(passwordStr);
-					passwordStrObj.password = passwordStrObj.password.replace(/^\$2y(.+)$/i, '$2a$1');
-					return bcrypt.compare(password, passwordStrObj.password, (err, res) => {
-						if(res){
-							return callback(false, true);
-						}else{
-							return callback(new Error('Wrong username or password'), false);
-						}
-					});
-				}else{
-					return callback(new Error('Wrong username or password'), false);
-				}
-			});
-			connection.release();
 		}
-	})
+		connection.query('SELECT password FROM users WHERE BINARY name = ?', [username], function(err, dbRes, fields){
+			if(err){
+				if(callback !== undefined){
+					var errObj = new Error(err.code);
+					errObj.name = 'server';
+
+					callback(errObj, false);
+				}else{
+					throw err;
+				}
+			// Valid username
+			}else if(dbRes.length > 0){
+				var bcrypt = require('bcrypt');
+				var passwordStr = JSON.stringify(dbRes[0]);
+				var passwordStrObj = JSON.parse(passwordStr);
+				passwordStrObj.password = passwordStrObj.password.replace(/^\$2y(.+)$/i, '$2a$1');
+				return bcrypt.compare(password, passwordStrObj.password, (err, res) => {
+					if(res){
+						return callback(false, true);
+					}else{
+						return callback(new Error('Wrong username or password'), false);
+					}
+				});
+			}else{
+				return callback(new Error('Wrong username or password'), false);
+			}
+		});
+		connection.release();
+	});
 }
--- www/lib/functions/register.func.js
+++ www/lib/functions/register.func.js
@@ -1,41 +1,41 @@
 module.exports = isUsed = function(name, email, callback){
-	getCon((err, con) => {
+	return getCon((err, con) => {
 		if(err){
 			var errObj = new Error(err.code);
 			errObj.name = 'server';
 
-			callback(errObj, false);
-		}else{
-			con.query('SELECT id FROM users WHERE BINARY name  = ?', [name], (dbError, dbResult) => {
-				if(dbError){
-					var errObj = new Error(dbError.code);
-					errObj.name = 'server';
-
-					callback(errObj, false);
-				// Valid username
-				}else if(dbResult.length === 0){
-					con.query('SELECT id From users WHERE email = ?', [email], (dbErr, dbRes) => {
-						if(dbErr){
-							var errObj = new Error(dbErr.code);
-							errObj.name = 'server';
-
-							callback(errObj, false);
-						}else if(dbRes.length === 0){
-							callback(false, true);
-						}else{
-							var errObj = new Error('This email address is already used');
-
-							callback(errObj, false);
-						}
-					})
-				}else{
-					var errObj = new Error('This username is already used');
-
-					callback(errObj, false);
-				}
-			})
+			return callback(errObj, false);
 		}
-	})
+		return con.query('SELECT id FROM users WHERE BINARY name  = ?', [name], (dbError, dbResult) => {
+			if(dbError){
+				var errObj = new Error(dbError.code);
+				errObj.name = 'server';
+
+				return callback(errObj, false);
+			// Valid username
+			}
+			if(dbResult.length === 0){
+				var crypto = require('crypto');
+				var shasum = crypto.createHash('sha512');
+				emailHash = shasum.update(email).digest('hex');
+				return con.query('SELECT id From users WHERE email = ?', [emailHash], (dbErr, dbRes) => {
+					if(dbErr){
+						var errObj = new Error(dbErr.code);
+						errObj.name = 'server';
+
+						return callback(errObj, false);
+					}
+					if(dbRes.length === 0){
+						return callback(false, true);
+					}else{
+						return callback(new Error('This email address is already used'), false);
+					}
+				});
+			}else{
+				return callback(new Error('This username is already used'), false);
+			}
+		});
+	});
 },checkFormats = function(name, email, callback){
 	namePattern = new RegExp(/^[a-zA-Z0-9_@[\]éè-]+$/);
 	emailPattern = new RegExp(/^[a-z0-9._-]+@[a-z0-9._-]{2,20}\.[a-z]{2,4}$/);
@@ -44,88 +44,70 @@ module.exports = isUsed = function(name, email, callback){
 	}else if(!emailPattern.test(email)){
 		return callback(new Error('Email address not valid'), false);
 	}else{
-		callback(false, true);
+		return callback(false, true);
 	}
 },register = function(name, email, password, repeatPassword, callback){
 	if(name === undefined){
-		var errObj = new Error('Please enter a username');
-
-		return callback(errObj, false);
+		return callback(new Error('Please enter a username'), false);
 	}
 	if(name.length < 4){
-		var errObj = new Error('Your username must be at least 4 chars long');
-
-		return callback(errObj, false);
+		return callback(new Error('Your username must be at least 4 chars long'), false);
 	}
 	if(email === undefined){
-		var errObj = new Error('Please enter an email address');
-
-		return callback(errObj, false);
+		return callback(new Error('Please enter an email address'), false);
 	}
 	if(password === undefined){
-		var errObj = new Error('Please enter a password');
-
-		return callback(errObj, false);
+		return callback(new Error('Please enter a password'), false);
 	}
 	if(password.length < 8){
-		var errObj = new Error('Your password must be at least 8 chars long');
-
-		return callback(errObj, false);
+		return callback(new Error('Your password must be at least 8 chars long'), false);
 	}
 	if(repeatPassword === undefined){
-		var errObj = new Error('Please repeat your password');
-
-		return callback(errObj, false);
+		return callback(new Error('Please repeat your password'), false);
 	}
 	if(password !== repeatPassword){
-		var errObj = new Error('The passwords are different');
-
-		return callback(errObj, false);
+		return callback(new Error('The passwords are different'), false);
 	}
-	getCon((dbErr, dbCon) => {
+	return getCon((dbErr, dbCon) => {
 		if(dbErr){
 			var errObj = new Error(dbErr.code);
 			errObj.name = 'server';
 
-			callback(errObj, false);
-		}else{
-			checkFormats(name, email, (error, result) => {
-				if(error){
-					callback(error, result);
-				}else if(result){
-					isUsed(name, email, (err, res) => {
-						if(err){
-							callback(err, false);
-						}else if(res){
-							var bcrypt = require('bcrypt');
-							bcrypt.hash(password, 10, (passErr, hash) => {
-								if(passErr){
-									var errObj = new Error(passErr.code);
-									errObj.name = 'server';
+			return callback(errObj, false);
+		}
+		return checkFormats(name, email, (error, result) => {
+			if(error){
+				return callback(error, result);
+			}
+			return isUsed(name, email, (err, res) => {
+				if(err){
+					return callback(err, false);
+				}
+				var bcrypt = require('bcrypt');
+				return bcrypt.hash(password, 10, (passErr, hash) => {
+					if(passErr){
+						var errObj = new Error(passErr.code);
+						errObj.name = 'server';
 
-									callback(errObj, false);
-								}else{
-									console.log('ok');
-									var crypto = require('crypto');
-									var shasum = crypto.createHash('sha512');
-									shasum.update(email);
-									dbCon.query('INSERT INTO users (name, email, password, reg_date) VALUES (?, \
-										?, ?, NOW())', [name, shasum.digest('hex'), hash], (queryError, dbResult) => {
-										if(queryError){
-											var errObj = new Error(queryError.code);
-											errObj.name = 'server';
+						return callback(errObj, false);
+					}
+					var crypto = require('crypto');
+					var shasum = crypto.createHash('sha512');
+					emailHash = shasum.update(email).digest('hex');
+					return dbCon.query('INSERT INTO users (name, email, password, reg_date) VALUES (?, \
+						?, ?, NOW())', [name, emailHash, hash], (queryError, dbResult) => {
+						if(queryError){
+							var errObj = new Error(queryError.code);
+							errObj.name = 'server';
 
-											callback(errObj, false);
-										}else{
-											callback(false, true);
-										}
-									})
-								}
-							})
+							return callback(errObj, false);
+						}else{
+							return callback(false, true);
 						}
 					})
-				}
+				})
 			})
-		}
+		})
+		dbCon.release();
 	})
 };
