111ee694823cd14d0684293cafe7de93afc83bed
f1she3 <f1sher@gmx.com>
2019-03-07 19:19:16 +0100

    Removed unused folder

--- www/lib/routes/logout.js
+++
@@ -1,9 +0,0 @@
-let express = require('express');
-let router = express.Router();
-
-router.get('/', (request, response) => {
-	request.session.username = undefined;
-	request.flash('success', 'Successfully logged out');
-	response.redirect('/login');
-});
-module.exports = router;
